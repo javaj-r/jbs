eea684ae6e423e41d7885cd3b3b3393fc681ece5
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-18 11:14:56 +0330

    Add BankRepository. Closes #35.

---
+++ model/src/main/java/com/javid/repository/BankRepository.java
@@ -0,0 +1,10 @@
+package com.javid.repository;
+
+import com.javid.model.Bank;
+
+/**
+ * @author javid
+ * Created on 1/17/2022
+ */
+public interface BankRepository extends CrudRepository<Bank, Long> {
+}
