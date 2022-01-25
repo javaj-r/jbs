d9d6ae7d1897cc10ee039f2ebca68c2ba8fe9895
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-25 17:58:20 +0330

    Refactor AccountRepository.

--- model/src/main/java/com/javid/repository/AccountRepository.java
+++ model/src/main/java/com/javid/repository/AccountRepository.java
@@ -2,9 +2,13 @@ package com.javid.repository;
 
 import com.javid.model.Account;
 
+import java.util.List;
+
 /**
  * @author javid
  * Created on 1/16/2022
  */
 public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    List<Account> findAllByCustomerId(Long customerId);
 }
