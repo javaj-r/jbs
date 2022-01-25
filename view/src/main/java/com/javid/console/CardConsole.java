86584407c62ad6947835aac1dd0c5d6f89123fe1
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-25 18:50:51 +0330

    Refactor CardConsole.

--- view/src/main/java/com/javid/console/CardConsole.java
+++ view/src/main/java/com/javid/console/CardConsole.java
@@ -2,9 +2,13 @@ package com.javid.console;
 
 import com.javid.model.Account;
 import com.javid.model.Card;
+import com.javid.service.AccountBalanceException;
+import com.javid.service.AuthenticationException;
+import com.javid.service.CardNumberException;
 import com.javid.service.CardService;
 import com.javid.util.Screen;
 
+import java.sql.Date;
 import java.util.List;
 
 /**
@@ -36,7 +40,8 @@ public class CardConsole {
                     , "Create card"
                     , "Update card"
                     , "Delete card"
-                    , "Set or change password");
+                    , "Set or change password"
+                    , "Transfer Money Card to Card");
 
             if (choice == 0)
                 break;
@@ -47,8 +52,72 @@ public class CardConsole {
                 case 3 -> updateCard();
                 case 4 -> deleteCard();
                 case 5 -> changePassword();
+                case 6 -> transferMoneyCardToCard();
             }
         }
+    }
+
+    private void selectCard() {
+        Card card1 = selectCard("Select from menu: ");
+
+        if (!card1.isNew()) {
+            card = card1;
+        }
+    }
+
+    private Card selectCard(String message) {
+        List<Card> cards = cardService.findAll();
+        String[] arr = cards.stream().map(Card::toString)
+                .toList()
+                .toArray(new String[0]);
+
+        int choice = Screen.showMenu("", "", message, "Invalid choice"
+                , "Cancel"
+                , arr);
+
+        if (choice == 0) {
+            return new Card();
+        }
+
+        return cards.get(choice - 1);
+    }
+
+    private void createCard() {
+        Account account = AccountConsole.getInstance().selectAccount("Select card account: ");
+        if (account == null || account.isNew())
+            return;
+        if (account.getCard() != null && !account.getCard().isNew()) {
+            Screen.printError("This account already has a card!");
+            return;
+        }
+
+        Card card1 = new Card()
+                .setAccount(account);
+        card1 = cardService.create(card1);
+        System.out.println(card1);
+        if (!card1.isNew()) {
+            card = card1;
+        }
+    }
+
+    private void updateCard() {
+        Card card1 = selectCard("Select card to update: ");
+        if (card1.isNew())
+            return;
+
+        int choice = Screen.showMenu("", "",
+                "Select from menu: ", "Invalid choice."
+                , "Disable card.", "Enable card.");
+
+        card1.setEnabled(choice != 0);
+        cardService.update(card1);
+    }
+
+    private void deleteCard() {
+        Card card1 = selectCard("Select card to delete: ");
+        if (card1.isNew())
+            return;
+        cardService.delete(card1);
     }
 
     private void changePassword() {
@@ -86,80 +155,22 @@ public class CardConsole {
         }
     }
 
-    private void selectCard() {
-        Card card1 = selectCard("Select from menu: ");
-
-        if (!card1.isNew()) {
-            card = card1;
-        }
-    }
-
-    private Card selectCard(String message) {
-        List<Card> cards = cardService.findAll();
-        String[] arr = cards.stream().map(Card::toString)
-                .toList()
-                .toArray(new String[0]);
-
-        int choice = Screen.showMenu("", "", message, "Invalid choice"
-                , "Cancel"
-                , arr);
-
-        if (choice == 0) {
-            return new Card();
-        }
-
-        return cards.get(choice - 1);
-    }
-
-    private void createCard() {
-        Account account = AccountConsole.getInstance().selectAccount("Select card account: ");
-        if (account == null || account.isNew())
-            return;
-        if (account.getCard() != null || !account.getCard().isNew()) {
-            Screen.printError("This account already has a card!");
-        }
-
-        Card card1 = new Card()
-                .setAccount(account);
-        card1 = cardService.create(card1);
-        if (!card1.isNew()) {
-            card = card1;
-        }
-    }
-
-    private void updateCard() {
-        Card card1 = selectCard("Select card to update: ");
-        if (card1.isNew())
-            return;
-
-        int choice = Screen.showMenu("", "",
-                "Select from menu: ", "Invalid choice."
-                , "Disable card.", "Enable card.");
-
-        card1.setEnabled(choice != 0);
-        cardService.update(card1);
-    }
-
-    private void deleteCard() {
-        Card card1 = selectCard("Select card to delete: ");
-        if (card1.isNew())
-            return;
-
-        cardService.delete(card1);
-    }
-
     private int getPassword1() {
+        int password;
         while (true) {
-            int password = Screen.getInt("Enter a 4 digit password or 0 to cancel: ", "Invalid number.");
-            if (password == 0 || (password >= 1000 && password < 10000))
-                return password;
+            int input = Screen.getInt("Enter a 4 digit password or 0 to cancel: ", "Invalid number.");
+            if (input == 0 || (input >= 1000 && input < 10000)) {
+                password = input;
+                break;
+            }
         }
+        return password;
     }
 
     private boolean testPassword1(Card card) {
         int counter = 0;
         while (true) {
-            int password = Screen.getInt("Enter password or 0 to cancel: ", "Invalid number.");
+            int password = Screen.getInt("Enter your password1 or 0 to cancel: ", "Invalid number.");
             if (password == 0)
                 return false;
 
@@ -168,13 +179,80 @@ public class CardConsole {
 
             if (++counter == 3) {
                 cardService.update(card.setEnabled(false));
+                System.out.println("Your card got disabled because of 3 wrong password");
                 return false;
             }
         }
     }
 
     public void transferMoneyCardToCard() {
+        long number1 = getCardByNumber("Enter your card number: ");
+        if (number1 == 0)
+            return;
+        long number2 = getCardByNumber("Enter destination card number: ");
+        if (number2 == 0)
+            return;
+        long amount = getAmount();
+        if (amount == 0)
+            return;
+        int password2 = Screen.getInt("Enter your password 2 or 0 to cancel: ", "Invalid number.");
+        if (password2 == 0)
+            return;
+        int cvv2 = Screen.getInt("Enter your cvv2 or 0 to cancel: ", "Invalid number.");
+        if (cvv2 == 0)
+            return;
+        Date expireDate = Screen.getFirstDayOfMonth("Enter year and mont of card expire", "Invalid date");
+        Card source = new Card()
+                .setNumber(number1)
+                .setPassword2(password2)
+                .setCvv2(cvv2)
+                .setExpireDate(expireDate);
 
+        Card destination = new Card()
+                .setNumber(number2);
+
+
+        try {
+            cardService.transferMoneyCardToCard(source, destination, amount);
+
+        } catch (AuthenticationException | AccountBalanceException e) {
+            Screen.printError(e.getMessage(), 500);
+        }
+
+    }
+
+    private long getCardByNumber(String message) {
+        while (true) {
+            try {
+                long number = Screen.getLong(message, "Invalid number.");
+                cardService.findByCardNumber(new Card().setNumber(number));
+                return number;
+            } catch (CardNumberException e) {
+                Screen.printError(e.getMessage(), 500);
+            }
+            int choice = Screen.showMenu("", ""
+                    , "Select from menu: ", "Invalid choice."
+                    , "Cancel", "Try again");
+            if (choice == 0)
+                break;
+        }
+        return 0;
+    }
+
+    private long getAmount() {
+        while (true) {
+            long amount = Screen.getLong("Enter 0 or amount: ", "Invalid number");
+            if (amount < 0) {
+                Screen.printError("Amount cannot be negative!", 500);
+                int choice = Screen.showMenu("", ""
+                        , "Select from menu: ", "Invalid choice."
+                        , "Cancel", "Try again");
+                if (choice == 0)
+                    break;
+            }
+            return amount;
+        }
+        return 0;
     }
 
 }
