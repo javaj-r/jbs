ef1a9bcd9a1aa6cae2e23b2716bf0d8184e380e3
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-25 17:54:55 +0330

    Add AuthenticationException.

---
+++ control/src/main/java/com/javid/service/AuthenticationException.java
@@ -0,0 +1,19 @@
+package com.javid.service;
+
+/**
+ * @author javid
+ * Created on 1/25/2022
+ */
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException() {
+    }
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
